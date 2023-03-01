489542c74214c9fad591614cc068698908df5b3a
baladium <113074099+kaladin99@users.noreply.github.com>
2023-03-01 15:17:45 +0100

    canvi1

--- Pool2.java
+++ Pool2.java
@@ -4,12 +4,19 @@ public class Pool2 extends Pool {   //no kids alone
     private int numkid = 0;
     private int numins = 0;
     //private int ratio = 0;
+    int ki_L;
+    int cap_L;
 
-    public void init(int ki, int cap){}
+    public void init(int ki, int cap){
+        ki_L = ki;
+        cap_L = cap;
+
+
+    }
 
     public synchronized void kidSwims() throws InterruptedException {
         
-        while( numkid >= (numins*(log.nk/log.ni)) ){ 
+        while( numkid >= ki_L ){ 
             
             wait();
             log.waitingToSwim();
@@ -39,7 +46,7 @@ public class Pool2 extends Pool {   //no kids alone
     } // al entrar un monitor ha de avisar als xiquets que volen entrar
     public synchronized void instructorRests() throws InterruptedException  {
         
-        while (numkid >= (numins * 2  - 1)){
+        while (numkid < ki_L){
             wait();
             log.waitingToRest();
             
